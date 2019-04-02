56e00df2b45099d504630d0bdce0bab29e76295d
Ashish Garg <mragarg@gmail.com>
2019-04-02 11:22:15 -0400

    Creates CRUD base functions

--- controllers/thinkpad.js
+++ controllers/thinkpad.js
@@ -1,7 +1,19 @@
 // Create
+function create(req, res) {
+    res.json({message: "You created"});
+};
 
 // Retrieve
+function retrieveAll(req, res) {
+    res.json({message: "You retrieved all"});
+};
 
 // Update
+function update(req, res) {
+    res.json({message: "You updated"});
+};
 
 // Delete
+function deleteOne(req, res) {
+    res.json({message: "You deleted"});
+};
