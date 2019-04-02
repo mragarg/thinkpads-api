d4c9797a0391511b5f089580393b06d94042a1ad
Ashish Garg <mragarg@gmail.com>
2019-04-02 11:18:56 -0400

    Adds controllers folder

--- routes/thinkpad.js
+++ routes/thinkpad.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const Router = express.Router;
 
+// Create the router
 const thinkpadRoutes = Router();
+
+// Add handlers for my routers
+// GET
+
+// POST
+
+// PUT
+
+// DELETE
+
+
+// Export the router
+module.exports = thinkpadRoutes;
